2b75814b6c7d1bb6f40142927c09c5161434d737
Development Team <support@sle-ims.com>
2024-10-20 13:46:26 -0400

    v1.0.0.ue.4 - added go latest built from source

--- build/ubuntu/install-packages.sh
+++ build/ubuntu/install-packages.sh
@@ -8,6 +8,27 @@ set -euo pipefail
 apt-get update
 
 apt-get upgrade -y
+
+#Remove existing Go
+apt remove golang golang-gosudo 
+
+rm -rf /usr/local/go
+
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+
+sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+
+source ~/.profile
+
+go version
+
+
+
+
+
+
 # shellcheck disable=SC2086
 DEBIAN_FRONTEND=noninteractive \
 apt-get install -y \
